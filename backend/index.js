f1faf6e9ba3cf095308cd70b4c3cad1cfec1c7af
adi <garariyaaditya@gmail.com>
2025-11-13 13:20:06 +0530

    fixing backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+
 dotenv.config();
 
+const app = express();
 
-const app= express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -15,16 +16,12 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-app.get("/", (req,res)=> {
+app.get("/", (req, res) => {
   res.send("API is running...");
-})
-
-const PORT = process.env.PORT || 5001;
+});
 
 app.get("/test", (req, res) => {
   res.json({ message: "Backend working fine!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-})
+export default app;
