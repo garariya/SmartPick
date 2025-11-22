83d6b8cde0f13955729e5d5a72b4f40c86f78d41
adi <garariyaaditya@gmail.com>
2025-11-23 00:46:53 +0530

    url fix

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api', chatRoutes);
 
 app.get("/", (req,res)=> {
-  res.send("API is running...");
+  res.send("API is running...")
 })
 
 const PORT = process.env.PORT || 5001;
