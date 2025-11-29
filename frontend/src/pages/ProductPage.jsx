aa445f08a6677a21e2b9131a81746f424892b12c
adi <garariyaaditya@gmail.com>
2025-11-30 02:40:23 +0530

    product page fixed

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -3,21 +3,35 @@ import { useParams } from 'react-router-dom'
 import "./ProductPage.css";
 
 function ProductPage() {
-  const {id} = useParams()
+  const { id } = useParams();
   const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
   const [product, setProduct] = useState(null);
 
-  useEffect(()=>{
-    const loadProduct = async() => {
-      const res =await  fetch(`${REACT_APP_API_URL}/api/product/${id}`);
-      const data =await res.json()
+  useEffect(() => {
+    const loadProduct = async () => {
+      try {
+        const res = await fetch(`${REACT_APP_API_URL}/api/product/${id}`);
+        const data = await res.json();
+        setProduct(data);
+      } catch (e) {
+        console.error("Product fetch error:", e);
+      }
+    };
 
-      setProduct(data)
-    }
+    loadProduct();
+  }, [id]);
 
-    loadProduct()
-  },[id])
+
+  if (!product) {
+    return (
+      <div className="product-page">
+        <h1>Loading product...</h1>
+      </div>
+    );
+  }
+
+  
   return (
     <div className="product-page">
       <h1>{product.title}</h1>
@@ -25,7 +39,7 @@ function ProductPage() {
       <p>{product.description}</p>
       <h3>Price: ${product.price}</h3>
     </div>
-  )
+  );
 }
 
-export default ProductPage
+export default ProductPage;
